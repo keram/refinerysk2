ee3a540f3a7a132303e605567093f956931f4d22
Marek Labos <keraml@gmail.com>
2014-02-10 23:01:07 +0100

    use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Refinerysk::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :sendmail
 end
